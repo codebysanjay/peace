76226b5f27e9e234e1f060895d846f3520d99ab2
SanjayMohan <sanjaykakkatiri@gmail.com>
2020-07-27 23:36:14 +0530

    Adding Buttons

--- lib/main.dart
+++ lib/main.dart
@@ -16,11 +16,19 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class Peace extends StatelessWidget {
+class Peace extends StatefulWidget {
+  @override
+  _PeaceState createState() => _PeaceState();
+}
+
+var play = true;
+
+class _PeaceState extends State<Peace> {
   @override
   Widget build(BuildContext context) {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
+
     return Scaffold(
       body: Container(
         height: height,
@@ -36,37 +44,136 @@ class Peace extends StatelessWidget {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: <Widget>[
-              Column(
+              Container(
+                child: Padding(
+                  padding:
+                      EdgeInsets.only(left: width * 0.1, top: height * 0.05),
+                  child: RichText(
+                    textAlign: TextAlign.start,
+                    text: TextSpan(
+                      children: [
+                        TextSpan(
+                          text: 'Welcome to,',
+                          style: TextStyle(
+                            fontSize: height * 0.04,
+                            color: Colors.white,
+                            fontFamily: 'Comfortaa',
+                          ),
+                        ),
+                        TextSpan(
+                          text: '\nPeace',
+                          style: TextStyle(
+                            fontSize: height * 0.065,
+                            color: Colors.white,
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: height * 0.15),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () {},
+                      child: Container(
+                        height: height * 0.08,
+                        width: height * 0.08,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset(
+                            'assets/rain.png',
+                          ),
+                        ),
+                      ),
+                    ),
+                    FlatButton(
+                      onPressed: () {},
+                      child: Container(
+                        height: height * 0.08,
+                        width: height * 0.08,
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(10)),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Image.asset(
+                            'assets/fishing.png',
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: <Widget>[
-                  Container(
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 30, top: 50),
-                      child: RichText(
-                        textAlign: TextAlign.start,
-                        text: TextSpan(
-                          children: [
-                            TextSpan(
-                              text: 'Welcome to,',
-                              style: TextStyle(
-                                fontSize: 30,
-                                color: Colors.white,
-                                fontFamily: 'Comfortaa',
-                              ),
-                            ),
-                            TextSpan(
-                              text: '\nPeace',
-                              style: TextStyle(
-                                fontSize: 50,
-                                color: Colors.white,
-                              ),
-                            )
-                          ],
+                  FlatButton(
+                    onPressed: () {},
+                    padding: EdgeInsets.only(top: height * 0.04),
+                    child: Container(
+                      height: height * 0.08,
+                      width: height * 0.08,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'assets/forest.png',
+                        ),
+                      ),
+                    ),
+                  ),
+                  FlatButton(
+                    padding: EdgeInsets.only(top: height * 0.04),
+                    onPressed: () {},
+                    child: Container(
+                      height: height * 0.08,
+                      width: height * 0.08,
+                      decoration: BoxDecoration(
+                          color: Colors.white,
+                          borderRadius: BorderRadius.circular(10)),
+                      child: Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Image.asset(
+                          'assets/water.png',
                         ),
                       ),
                     ),
                   ),
                 ],
               ),
+              Padding(
+                padding: EdgeInsets.only(top: height * 0.2),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    FlatButton(
+                      onPressed: () {
+                        setState(() {
+                          play = !play;
+                        });
+                      },
+                      child: Container(
+                        child: Image.asset(
+                          play ? 'assets/play-button.png' : 'assets/pause.png',
+                          color: Colors.white,
+                          height: width * 0.14,
+                          width: width * 0.14,
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              )
             ],
           ),
         ),
@@ -74,7 +181,3 @@ class Peace extends StatelessWidget {
     );
   }
 }
-//style: TextStyle(
-//fontSize: 50,
-//color: Colors.white,
-//),
